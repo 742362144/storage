1301444d2560f5e2f6274efb2f26b752e671962c
742362144 <742362144@qq.com>
2020-05-09 16:34:50 +0800

    support docker

--- build.sh
+++ build.sh
@@ -1,14 +1,1 @@
-#!/usr/bin/env bash
-
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-cd $SHELL_FOLDER
-
-protoc --python_out=. cmdcall.proto
-
-python -m py_compile *.py
-
-cp -rf *.pyc Dockerfile docker/
-
-
-cd docker
-docker build -t mybench .
+#!/usr/bin/env bashSHELL_FOLDER=$(cd "$(dirname "$0")";pwd)cd $SHELL_FOLDERprotoc --python_out=. cmdcall.protopython3 -m py_compile *.pycp -rf *.pyc Dockerfile docker/cd dockerdocker build -t mybench .
